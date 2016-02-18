31f7352ee85074c9bbb0f428be636ec1a8425c68
astroh <izzyrael@me.com>
2016-02-18 16:45:54 -0500

    first commit

---
+++ Native/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Native
+//
+//  Created by Izzy Rael on 2/18/16.
+//  Copyright © 2016 Team PlusPlus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController1: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
